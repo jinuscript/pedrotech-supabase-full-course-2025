8635a95c453b33ddd436ca240c30b8f43dfe2110
rlawlsdn263 <rlawlsdn263@naver.com>
2025-04-26 16:01:12 +0900

    FEAT: TaskBoard 컴포넌트 생성 및 tasks 조회 기능 구현

--- src/components/TaskBoard/TaskBoard.tsx
+++ src/components/TaskBoard/TaskBoard.tsx
@@ -1,5 +1,47 @@
+import { useState, useEffect } from "react";
+import { supabaseClient } from "@/services";
 import s from "./TaskBoard.module.css";
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+}
+
 export const TaskBoard = () => {
-  return <div className={s.TaskBoard}>보드</div>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const fetchTasks = async () => {
+    // supabase로 GET 통신
+    const { error, data } = await supabaseClient
+      .from("tasks")
+      .select("*")
+      .order("created_at", { ascending: true });
+
+    // 에러핸들링
+    if (error) {
+      console.error("할일호출 중 에러: ", error.message);
+      return;
+    }
+
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <ol className={s.TaskBoard}>
+      {tasks.map((task) => {
+        return (
+          <li key={task.id}>
+            <p>{task.title}</p>
+            <p>{task.description}</p>
+          </li>
+        );
+      })}
+    </ol>
+  );
 };
