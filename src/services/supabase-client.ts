db28d89b4a96b851a43b2a10858f84a2a0b493b7
rlawlsdn263 <rlawlsdn263@naver.com>
2025-04-26 15:29:00 +0900

    CHORE: supabase-client.ts 파일 위치를 /services로 이동

--- src/services/supabase-client.ts
+++ src/services/supabase-client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const VITE_SUPABASE_PROJECT_URL = import.meta.env.VITE_SUPABASE_URL;
+const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const VITE_SUPABASE_API_KEY = import.meta.env.VITE_SUPABASE_API_KEY;
 
-export const supabase = createClient(
-  VITE_SUPABASE_PROJECT_URL,
+export const supabaseClient = createClient(
+  VITE_SUPABASE_URL,
   VITE_SUPABASE_API_KEY
 );
