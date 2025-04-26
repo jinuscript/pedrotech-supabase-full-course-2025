b4036ec09e9226b782e9a6d86ea6cf80bd3a09bb
rlawlsdn263 <rlawlsdn263@naver.com>
2025-04-26 14:49:22 +0900

    FEAT: TaskForm 입력 핸들러 handleChange 분리 및 상태 관리 개선

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import s from "./TaskForm.module.css";
 
 export const TaskForm = () => {
+  const [task, setTask] = useState({ title: "", description: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setTask((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <form className={s.TaskForm}>
       {/* 제목 */}
-      <input className={s.input} type="text" />
+      <input
+        className={s.input}
+        type="text"
+        name="title"
+        value={task.title}
+        onChange={handleChange}
+      />
       {/* 설명 */}
-      <input className={s.input} type="text" />
+      <input
+        className={s.input}
+        type="text"
+        name="description"
+        value={task.description}
+        onChange={handleChange}
+      />
 
       <button className={s.button}>등록하기</button>
     </form>
