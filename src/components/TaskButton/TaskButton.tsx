2f638cf342d0907c772c47c117f5236498e4797c
rlawlsdn263 <rlawlsdn263@naver.com>
2025-04-26 16:24:52 +0900

    FEAT: tasks 업데이트 기능 구현

--- src/components/TaskButton/TaskButton.tsx
+++ src/components/TaskButton/TaskButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { supabaseClient } from "@/services";
 import s from "./TaskButton.module.css";
 
@@ -7,6 +8,8 @@ interface TaskButton {
 }
 
 export const TaskButton = ({ type, id }: TaskButton) => {
+  const [newDescription, setNewDescription] = useState("");
+
   // DELETE 기능 수행
   const deleteTask = async (id: number) => {
     const { error } = await supabaseClient.from("tasks").delete().eq("id", id);
@@ -17,8 +20,33 @@ export const TaskButton = ({ type, id }: TaskButton) => {
     }
   };
 
+  const updateTask = async (id: number) => {
+    const { error } = await supabaseClient
+      .from("tasks")
+      .update({ description: newDescription })
+      .eq("id", id);
+
+    if (error) {
+      console.error("업데이트 중 에러 발생: ", error.message);
+      return;
+    }
+
+    setNewDescription("");
+  };
+
   if (type === "수정") {
-    return <button className={s.TaskButton}>수정</button>;
+    return (
+      <div>
+        <input
+          type="text"
+          value={newDescription}
+          onChange={(e) => setNewDescription(e.target.value)}
+        />
+        <button className={s.TaskButton} onClick={() => updateTask(id)}>
+          수정
+        </button>
+      </div>
+    );
   }
 
   if (type === "삭제") {
